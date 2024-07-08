990590f2178cf5277ae399c1dc9e4ad0289ac7cb
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-07-08 05:34:05 +0000

    [Add]一覧画面の各リンクを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,15 +7,23 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book.id)
+    redirect_to '/books'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/'
+  end
+
+  private
   def book_params
     params.require(:book).permit(:title, :body)
   end
